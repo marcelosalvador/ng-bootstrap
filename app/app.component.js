3e39f7666e52102d385e2a94203da9bd045961b0
Marcelo Salvador <c-marcelo.salvador@charter.com>
2017-02-24 16:18:04 -0500

    Model Binding

--- app/app.component.js
+++ app/app.component.js
@@ -7,15 +7,18 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
+/** Author: Marcelo Salvador */
+/**Declaring a model binding to the element */
 var AppComponent = (function () {
     function AppComponent() {
+        this.name = 'World';
     }
     return AppComponent;
 }());
 AppComponent = __decorate([
     core_1.Component({
         selector: 'my-app',
-        template: '<h1> Hello, World</h1>'
+        template: "\n    <h1>Hello, {{name || 'World'}}</h1>\n    <input type=\"text\" [(ngModel)] = \"name\" placeholder=\"name\">\n\n  "
     })
 ], AppComponent);
 exports.AppComponent = AppComponent;
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
+/** Author: Marcelo Salvador */
+/**Declaring a model binding to the element */
 @Component({
   selector: 'my-app',
-  template: '<h1> Hello, World</h1>'})
-export class AppComponent {}
+  template: `
+    <h1>Hello, {{name || 'World'}}</h1>
+    <input type="text" [(ngModel)] = "name" placeholder="name">
+
+  `})
+export class AppComponent {
+  name: string = 'World';
+}
